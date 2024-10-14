9f95ba78ece1a69e0ada38d5968aee9123fa5453
TeixeiraManuel <teixeiramanuelteidy@gmail.com>
2024-10-14 19:04:43 -0300

    feat:finish project

--- js/script.js
+++ js/script.js
@@ -1,17 +1,75 @@
 let getMap = document.querySelector('#map')
+let searchValue = document.querySelector('#search')
+let btnSearch = document.querySelector('.btn')
+let addressDetails = document.querySelectorAll('.address-details')
+let marker;
 var map;
-var iconUrl
-map = L.map(getMap).setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 100,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+initialRequest()
 
+async function initialRequest(){
 
-var myIcon = L.icon({
-    iconUrl: '../images/icon-location.svg',
-    iconSize: [38, 95],
+  try {
+    const response = await fetch ("http://ip-api.com/json/");
+    const data = await response.json();
+    const {lat, lon} = data
 
-});
-L.marker([51.505, -0.09], {icon: myIcon}).addTo(map);
+    modalInformation(data.query, data.city, data.timezone, data.isp)
+
+    map = L.map(getMap).setView([lat, lon], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+  
+  // Cria um novo marcador
+    marker = L.marker([lat, lon])
+      .addTo(map)
+  } catch (error) {
+    alert('Erro ao obter os dados')
+  }
+}
+
+btnSearch.addEventListener('click',()=>{
+  if(!searchValue.value){
+    alert('digite alguma coisa')
+  }else{
+    getLocation(searchValue.value)
+  }
+})
+
+async function getLocation(value) {
+  
+  try {
+    const response = await fetch(`http://ip-api.com/json/${value}`);
+    const data = await response.json();
+    const {lat, lon} = data;
+    
+    modalInformation(data.query, data.city, data.timezone, data.isp)
+    createMarker(lat, lon)
+  } catch (error) {
+      console.log('Erro ao Obter os dados:', error.message);
+      alert('tente novamente')
+  }
+}
+
+function createMarker(lat, lon){
+  if (marker) {
+    map.removeLayer(marker);
+}
+
+// Cria um novo marcador
+  marker = L.marker([lat, lon])
+    .addTo(map)
+    .bindPopup('Você está em')
+    .openPopup();
+  map.setView([lat, lon], 13);
+}
+
+function modalInformation(ip,city,timeZone,isp){
+  addressDetails[0].textContent = ip || 'Indisponível';
+  addressDetails[1].textContent = city || 'Indisponível';
+  addressDetails[2].textContent = timeZone || 'Indisponível';
+  addressDetails[3].textContent = isp || 'Indisponível';
+}
